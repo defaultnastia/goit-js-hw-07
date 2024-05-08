fb20aaac346bbb284279fffc8ac128ac4e62bc8a
defaultnastia <anastayaaa@gmail.com>
2024-05-08 13:46:26 +0200

    added task5
    and removed comments

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,6 @@
 const categoriesArray = document.querySelectorAll('li.item');
 
-// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-
 console.log(`Number of categories: ${categoriesArray.length}`);
-
-// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
 
 categoriesArray.forEach(category => {
   const categoryNameText = category.querySelector('h2').textContent;
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const changeColorBtnEl = bodyEl.querySelector('.change-color');
+const colorOutputTextEl = bodyEl.querySelector('.color');
+
+function onChangeColorBtnClick() {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+
+  colorOutputTextEl.textContent = randomColor;
+}
+
+changeColorBtnEl.addEventListener('click', onChangeColorBtnClick);
